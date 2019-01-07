afaf54b488915217e7b27ca020deb9477c7b5f94
Bilal Khan <dijanmatheson@gmail.com>
2019-01-07 15:08:45 +0000

    Git init

---
+++ src/main/scala/Recursion2.scala
@@ -0,0 +1,15 @@
+object Recursion2 extends App {
+
+  var count = 0
+
+  def recur(word:String,times:Int){
+    println(word*times)
+    count +=1
+
+    if(count<times) recur(word,times)
+
+  }
+
+  recur("h",5)
+
+}
