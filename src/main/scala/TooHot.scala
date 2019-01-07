d8710b8fc5ec5883ac3f4a07a6ea08624b571309
Bilal Khan <dijanmatheson@gmail.com>
2019-01-07 17:34:43 +0000

    Git init

--- src/main/scala/TooHot.scala
+++ src/main/scala/TooHot.scala
@@ -1,8 +1,8 @@
 object TooHot extends App{
 
-  def climate (temperature:Int, isSummer:Boolean):String = {
-    if (isSummer==true && temperature > 60 ) print("true")
-    else if (isSummer==false && temperature >60 ) print("true")
+  def climate (temperature:Int, isSummer:Boolean):Unit = {
+    if (isSummer==true && temperature > 60 && temperature<=100) print("true")
+    else if (isSummer==false && temperature >60 && temperature<=90 ) print("true")
     else print("false")
   }
   climate(100,true)
