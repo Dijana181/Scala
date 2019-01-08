c4e633f131789f59a188438e4ada9d3fa4aa3411
Bilal Khan <dijanmatheson@gmail.com>
2019-01-08 17:35:05 +0000

    Git init

--- src/main/scala/Bottles.scala
+++ src/main/scala/Bottles.scala
@@ -1,17 +1,14 @@
 object Bottles extends App {
 
-  var count = 100
-  var num1 = 100
 
-  def lyrics(){
 
-    count = count - 1
-    num1 = num1 -1
+  def lyrics(count:Int=100, num1:Int=100){
+
     var num2 = num1-1
 
     if(count>0) {
       println(s" $num1 bottles of beer on the wall, $num1 bottles of beer. Take one down, pass it around, $num2 bottles of beer on the wall")
-      lyrics()
+      lyrics(count-1, num1-1)
     }
 
   }
