df8878fad91e47051cf7fe84465026fd9d3ad6e8
Bilal Khan <dijanmatheson@gmail.com>
2019-01-09 16:04:47 +0000

    Adressed comments and fixed all of the issues

--- src/main/scala/BrokenKeyboard.scala
+++ src/main/scala/BrokenKeyboard.scala
@@ -1,31 +1,34 @@
 object BrokenKeyboard extends App {
 
-  def availableKeys (letters:String): Unit ={
-    for(i <- 0 to letters.length-1) letters(i)
+  def getWord() {
+    // val source = io.Source.fromFile("Users/Admin/Documents/DijanR/scala/enable1.txt")
+    //for(item <- source)
+    //for (r <- 0 until readlines.length) yield (r)
   }
 
-  def anagram (word:String, letters:String): Unit ={
-    var count = 0
-    var index = availableKeys(letters)
 
-    for(j <- 0 to word.length-1){
+  def anagram (letters:String, count:Int = 0): Unit = {
+    val readlines = List("abcd", "abcd", "abcd")
+    for (k<- 0 until readlines.length; j <- 0 until readlines(k).length; i <- 0 until letters.length) {
+      if (readlines(k).substring(j, j+1) == letters.substring(i,i+1)) count+1
 
-      if(word.substring(j,j+1)== index){
-        count = count+1
+      if (count == readlines(k).length) {
+        true
+        print(s"$readlines(k), ")
+        longestWord(readlines(k))
       }
-      if (count == word.length){
-        print("true")
-      }
-
-
     }
-    anagram("abcd", "abcd")
-
-
   }
 
-  def longestWord (): Unit ={
-
+  def longestWord(word:String):Unit ={
+    var maxWord = ""
+    if(word.length > maxWord.length){
+      maxWord = word
+    }
+    print(s"The longest word is $maxWord")
   }
+
+  anagram("abcd")
+
 
 }
