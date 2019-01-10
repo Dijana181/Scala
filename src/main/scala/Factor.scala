a1ee657011d122d830176fe599e0c3f8be7e54b3
Bilal Khan <dijanmatheson@gmail.com>
2019-01-10 17:49:42 +0000

    Merge branch 'master' of https://github.com/Dijana181/Scala into Thursday
    # Conflicts:
    #	src/main/scala/BrokenKeyboard.scala
    #	src/main/scala/NumberCheck.scala

---
+++ src/main/scala/Factor.scala
@@ -0,0 +1,30 @@
+object Factor extends App {
+
+  def factorial (limit:Int): Unit = {
+    var num = 1
+    for (i <- 1 to limit) {
+      num = num * i
+      num
+    }
+
+  }
+
+    def cap(): Unit ={
+      var limit = 120
+      var count = 1
+      var x = factorial(count)
+
+      while( x <=limit) {
+
+        count = count + 1
+      }
+
+      print(count)
+
+    }
+
+
+
+  cap()
+
+}
