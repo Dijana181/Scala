bc7bed77d7ed14249373c50eafea73db88645e54
Bilal Khan <dijanmatheson@gmail.com>
2019-01-10 10:11:03 +0000

    Adressed comments and fixed all of the issues

--- src/main/scala/NumberCheck.scala
+++ src/main/scala/NumberCheck.scala
@@ -1,25 +1,33 @@
 object NumberCheck extends App {
 
-  def addDigit(num1:String): Unit ={
+  def addDigit(num1:Int): Unit ={
 
     var num2 = num1.toString
     var num3 = ""
-    var num4 = ""
     var sum = 0
+    var num5 = 0
 
-    for(i <- 0 until num2.length by -2){
-      num3 =  num2.substring(i,i+1)
-      num3.toInt
-      sum = sum +(num3)
-      num3 =0
+    sum += (num1 %10)
+
+    for(i <- (num2.length-1) until 1 by -2 ) {
+      num3 += num2.substring(i - 1, i)
+      num5 = num3.toInt
+      sum += (num5*2)
+      num3 = ""
     }
 
-    for(j <- 0 until num2.length by -2){
-      num4 = num2.substring(j,j+1)
-      num4.toInt
-      sum =sum+(num4*2)
+    for(j <- (num2.length-2) until 0 by -2){
+      num3 = num2.substring(j-1,j)
+      num5 = num3.toInt
+      sum += (num5)
+      num3 = ""
     }
-    print(sum)
+print(sum)
   }
 addDigit(5555555)
+}
+
+def valid(num:Int):Unit = {
+
+}
 }
