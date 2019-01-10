8ef0e48c6028c904d89041d6d19323547f534078
Bilal Khan <dijanmatheson@gmail.com>
2019-01-10 15:00:02 +0000

    Adressed comments and fixed all of the issues

--- src/main/scala/PrimeNumbers.scala
+++ src/main/scala/PrimeNumbers.scala
@@ -1,17 +1,21 @@
-object PrimeNumbers {
+object PrimeNumbers extends App {
 
   def Prime (num1:Int): Unit ={
     var check = 0
     var count = 0
-    for(i <- 0 to num1){
+    for(i <- 1 to num1){
       if(num1 % i == 0) check+=1
     }
-    if(count==2) {
+    if(check==2) {
       count+=1
       PrimeCount(num1)
     }
   }
+
   def PrimeCount(x:Int): Unit ={
+    var count = 0
+    count += 1
+    println(s"There are $count prime numbers")
 
   }
 Prime(5)
