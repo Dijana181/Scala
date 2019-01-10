28389d770767fa3c425b987005b6b74bd19d7223
Bilal Khan <dijanmatheson@gmail.com>
2019-01-10 16:06:24 +0000

    Updated the Broken Keyboard with a link to the source. Improved the accuracy of the anagram function

--- src/main/scala/BrokenKeyboard.scala
+++ src/main/scala/BrokenKeyboard.scala
@@ -3,27 +3,29 @@ import scala.collection.mutable.ArrayBuffer
 
 object BrokenKeyboard extends App {
 
-  def getWord(readlines:String) {
-    var ArrayofWords = Source.fromFile("/Users/Admin/Documents/DijanR/scala/enable1.txt").getLines.toArray
+  def getWords() = {
+    Source.fromFile("/Users/Admin/Documents/DijanR/scala/enable1.txt").getLines.toArray
   }
 
   def anagram (letters:String): Unit = {
-    var readlines = "abcd"
+    val arrayofWords = getWords()
     var count: Int = 0
     val keyboards = ArrayBuffer[String]()
 
-      for (i <- 0 until letters.length; j <- 0 until readlines.length) {
-        if (letters.substring(i, i + 1) == readlines.substring(j, j + 1)) count += 1
+    for (word <- arrayofWords) {
+      for (i <- 0 until letters.length; j <- 0 until word.length) {
+        if (letters.substring(i,i+1) == word.substring(j, j+1)) count += 1
       }
-      if (count == readlines.length) {
-        keyboards.append(readlines)
+      if(count == word.length){
+        keyboards.append(word)
       }
-      println(keyboards.sortBy(_.length).last)
+      count = 0
     }
-  anagram("abcd")
+    print(s"$letters = ")
+    println(keyboards.sortBy(_.length).last)
+  }
+  anagram("abcdefg")
 }
-
-
 
 
 
