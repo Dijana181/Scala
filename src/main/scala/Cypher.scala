ee9e434a90241b7a2aa82d535a4621740c6ea13d
Bilal Khan <dijanmatheson@gmail.com>
2019-01-11 10:51:09 +0000

    Returns the factorial for a specified number

---
+++ src/main/scala/Cypher.scala
@@ -0,0 +1,28 @@
+object Cypher extends App {
+
+  def lookUp(letter:String): String = {
+
+    val map = Map("a" -> "z", "b" -> "y", "c" -> "x", "d" -> "w", "e" -> "v", "f" -> "u", "g" -> "t",
+      "h" -> "s", "i" -> "r", "j" -> "q", "k" -> "p", "l" -> "o", "m" -> "n", "n" -> "m", "o" -> "l",
+      "p" -> "k", "q" -> "j", "r" -> "i", "s" -> "h", "t" -> "g", "u" -> "f", "v" -> "e", "w" -> "d", "x" -> "c", "y" -> "b", "z" -> "a")
+
+    map(letter)
+
+  }
+
+
+  def reassignment(word:String): Unit ={
+
+    var newWord = ""
+
+    for (i<- 0 until word.length){
+     newWord+= lookUp(word.substring(i,i+1))
+    }
+
+    print(newWord)
+
+  }
+
+  reassignment("hello")
+
+}
