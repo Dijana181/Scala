46d7037b80ac9e6841bca487ab09e958738d9a96
Bilal Khan <dijanmatheson@gmail.com>
2019-01-11 09:37:26 +0000

    Returns the factorial for a specified number

--- src/main/scala/Factor.scala
+++ src/main/scala/Factor.scala
@@ -1,29 +1,24 @@
 object Factor extends App {
 
-  def factorial (limit:Int): Unit = {
+  def factorial(limit:Int): Unit ={
     var num = 1
-    for (i <- 1 to limit) {
-      num = num * i
-      num
-    }
 
+    for (i <- 1 to 20){
+      num = num * i
+      check(i, num, limit)
+    }
   }
 
-    def cap(): Unit ={
-      var limit = 120
-      var count = 1
-      var x = factorial(count)
-
-      while( x <=limit) {
-
-        count = count + 1
-      }
-      print(count)
-
+  def check(factorial:Int, num:Int, limit:Int): Unit ={
+    if(num>limit) {
+      print("Invalid factorial")
+      sys.exit()}
+    if(num==limit){
+      print(s"$factorial !")
+      sys.exit()
     }
+  }
 
+  factorial(479001600)
 
-
-  cap()
-
-}
+  }
