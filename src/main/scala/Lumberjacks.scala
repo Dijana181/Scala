eb75ae0a3be0995bf81acb62619f4ce236113316
Bilal Khan <dijanmatheson@gmail.com>
2019-01-13 14:37:06 +0000

    Sorted Array Application

--- src/main/scala/Lumberjacks.scala
+++ src/main/scala/Lumberjacks.scala
@@ -5,18 +5,28 @@ object Lumberjacks extends App {
   var Row3 = Array(1,4,1)
   var Storage = Array(Row1, Row2, Row3)
   var logs = 5
-  var min = Row1.reduceLeft(_ min _)
+  var min = 1
 
-  while (logs >= 0) {
-    for (row<- Storage) {
-      for (i <- 0 until row.length) {
-        if (row(i) == min) {
+  while(min<=9){
+    counter()
+    min = min +1
+
+  }
+
+
+def counter() {
+  for (row <- Storage) {
+    for (i <- 0 until row.length) {
+      if (row(i) == min) {
+        if(logs > 0) {
           row(i) += 1
+          logs = logs-1
         }
       }
     }
-    logs =logs-1
   }
+
+}
 
   for(i <- 0 until Row1.length) print(Row1(i))
   print("\n")
